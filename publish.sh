cad1aa708f41c1e3086705dce295ad408117a9ce
Kam <hello@sourcey.com>
2025-10-02 16:48:17 +1000

    chore: remove legacy gitbook commands from publish script

--- publish.sh
+++ publish.sh
@@ -1,7 +1,3 @@
-# gitbook build
-# cp -R _book/* publish
-# cd publish
-# git init
 git add -A
 git commit -m 'Update docs'
 git push git@github.com:tommyassociates/tommyassociates.github.io master --force
